4b1f578d0ba52c08627a75dd624140ac51a74d39
Astamp9 <andrew_de_stamps@yahoo.com>
2025-03-31 12:48:58 -0400

    initial api player gets updated

--- api/server.js
+++ api/server.js
@@ -1,11 +1,42 @@
 const express = require('express')
 const app = express();
 const port = 8081;
-const knex = require('knex')(require('../knexfile.js')["development"])
+const knex = require('knex')(require('./knexfile.js')["development"])
 
 app.get('/', (req, res) =>{
     res.send('Rival Teams Server is under construction')
 })
+
+//------------ players GET----------------------------
+
+app.get('/players', (req, res) =>{
+    knex('players')
+        .select('*')
+        .then(data => res.json(data))
+        .catch(function (error) {
+            console.error("Failed to find players", error);
+            res.status(500).json({ error: "Something went wrong" });
+        })
+});
+
+app.get('/players/:id', (req, res) =>{
+    let getId = req.params.id
+    knex('players')
+        .select('*')
+        .where({'id' : parseInt(getId)})
+        .then(player => {
+            if (player.length === 0){
+                res.status(404).json({error: "player not found"});
+            } else {
+                res.json(player)
+            }
+        })
+        .catch(function (error) {
+            console.error("Failed to find player", error);
+            res.status(500).json({ error: "Something went wrong" });
+            })
+        })
+
 
 app.listen(port, () => {
     console.log(`server is running on port ${port}`)
