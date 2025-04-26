e20d535869ac11ac7cf2722f79b6c600eaf90ce2
Astamp9 <andrew_de_stamps@yahoo.com>
2025-04-26 18:02:48 -0400

    refactor backend player teams to routes

---
+++ api/routes/playerTeams.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const router = express.Router();
+const knex = require('knex')(require('../knexfile.js')["development"]);
+
+router.get('/player_teams', (req, res) =>{
+    knex('player_team')
+        .select('*')
+        .then(data => res.json(data))
+        .catch(function (error) {
+            console.error("Failed to find teams", error);
+            res.status(500).json({ error: "Something went wrong" });
+        })
+});
+
+router.get('/player_teams/:id', (req, res) =>{
+    let getId = req.params.id
+    knex('player_team')
+        .select('*')
+        .where({'id' : parseInt(getId)})
+        .then(player_team => {
+            if (player_team.length === 0){
+                res.status(404).json({error: "player team link not found"});
+            } else {
+                res.json(player_team)
+            }
+        })
+        .catch(function (error) {
+            console.error("Failed to find player team link", error);
+            res.status(500).json({ error: "Something went wrong" });
+            })
+        })
+
+
+router.post('/player_teams', (req, res) => {
+    const {id, player_id, team_id} = req.body
+
+    knex('player_team')
+        .insert({id, player_id, team_id})
+        .returning('id')
+        .then(([id]) => {
+            res.status(201).json({success: true, id, message: 'player added to team'})
+        })
+        .catch(function (error) {
+            console.error("Failed to add player to team", error);
+            res.status(500).json({ error: "Something went wrong" });
+        })
+})
+
+router.delete('/player_teams/:id', (req, res) => {
+    let getId = req.params.id
+
+    knex('player_team')
+        .where({"id" : getId} )
+        .del()
+        .then(function(playerTeamExist){
+            if (playerTeamExist === 0) {
+                res.status(404).json({error: 'link not'})
+            } else {
+            res.status(200).json({success: true, id: getId, message: 'player team link deleted'})
+            }
+        })
+        .catch(function (error) {
+            console.error("Failed to delete link", error);
+            res.status(500).json({ error: "Something went wrong" });
+        })
+})
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -7,6 +7,7 @@ const playersRouter = require('./routes/players');
 const charactersRouter = require('./routes/characters');
 const characterCompsRouter = require('./routes/characterComps.js')
 const teamsRouter = require('./routes/teams.js')
+const playerTeamsRouter = require('./routes/playerTeams.js')
 
 app.use(cors());
 app.use(express.json());
@@ -19,72 +20,8 @@ app.use('/players', playersRouter);
 app.use('/characters', charactersRouter);
 app.use('/character_comps', characterCompsRouter)
 app.use('/teams', teamsRouter)
+app.use('/player_teams', playerTeamsRouter)
 
-
-//------------------------player_team CRUD----------------------------------
-
-app.get('/player_teams', (req, res) =>{
-    knex('player_team')
-        .select('*')
-        .then(data => res.json(data))
-        .catch(function (error) {
-            console.error("Failed to find teams", error);
-            res.status(500).json({ error: "Something went wrong" });
-        })
-});
-
-app.get('/player_teams/:id', (req, res) =>{
-    let getId = req.params.id
-    knex('player_team')
-        .select('*')
-        .where({'id' : parseInt(getId)})
-        .then(player_team => {
-            if (player_team.length === 0){
-                res.status(404).json({error: "player team link not found"});
-            } else {
-                res.json(player_team)
-            }
-        })
-        .catch(function (error) {
-            console.error("Failed to find player team link", error);
-            res.status(500).json({ error: "Something went wrong" });
-            })
-        })
-
-
-app.post('/player_teams', (req, res) => {
-    const {id, player_id, team_id} = req.body
-
-    knex('player_team')
-        .insert({id, player_id, team_id})
-        .returning('id')
-        .then(([id]) => {
-            res.status(201).json({success: true, id, message: 'player added to team'})
-        })
-        .catch(function (error) {
-            console.error("Failed to add player to team", error);
-            res.status(500).json({ error: "Something went wrong" });
-        })
-})
-
-app.delete('/player_teams/:id', (req, res) => {
-    let getId = req.params.id
-
-    knex('player_team')
-        .where({"id" : getId} )
-        .del()
-        .then(function(playerTeamExist){
-            if (playerTeamExist === 0) {
-                res.status(404).json({error: 'link not'})
-            } else {
-            res.status(200).json({success: true, id: getId, message: 'player team link deleted'})
-            }
-        })
-        .catch(function (error) {
-            console.error("Failed to delete link", error);
-            res.status(500).json({ error: "Something went wrong" });
-        })
-})
 
 //------------------player_proficiency CRUD------------------------------
 
